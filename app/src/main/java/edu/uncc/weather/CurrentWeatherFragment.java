264aed01bfb726c690e15b26047bd15561e6de34
Stephanie <slee185@uncc.edu>
2022-10-07 11:39:10 -0400

    implement gson

--- app/src/main/java/edu/uncc/weather/CurrentWeatherFragment.java
+++ app/src/main/java/edu/uncc/weather/CurrentWeatherFragment.java
@@ -16,9 +16,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 import edu.uncc.weather.databinding.FragmentCurrentWeatherBinding;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class CurrentWeatherFragment extends Fragment {
     private static final String ARG_PARAM_CITY = "ARG_PARAM_CITY";
@@ -59,6 +68,29 @@ public class CurrentWeatherFragment extends Fragment {
 
         TextView textViewCityName = binding.textViewCityName;
         textViewCityName.setText(mCity.getCity());
+
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={b6293da957857aa018c64d4783dad874}")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                requireActivity().runOnUiThread(() -> Toast.makeText(requireActivity(), "Unable to retrieve current weather from the Internet", Toast.LENGTH_LONG).show());
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    requireActivity().runOnUiThread(() -> Toast.makeText(requireActivity(), "Unable to retrieve current weather from the Internet", Toast.LENGTH_LONG).show());
+                    return;
+                }
+
+                Gson gson = new Gson();
+
+            }
+        });
 
         binding.buttonCheckForecast.setOnClickListener(new View.OnClickListener() {
             @Override
